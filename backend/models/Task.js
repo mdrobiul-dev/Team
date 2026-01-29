1e92e69328b6fc23dc857c69c1a31774ae3db82c
its-robii <robiulhassanrobi33@gmail.com>
2026-01-29 13:16:29 +0600

    All models created

---
+++ backend/models/Task.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    list: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "List",
+      required: true,
+    },
+    assignedTo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    order: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Task", taskSchema);
